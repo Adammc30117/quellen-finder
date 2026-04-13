10698a5e0139944de0320d14c88bfaf66d972e41
Adammc30117 <147706709+Adammc30117@users.noreply.github.com>
2026-04-13 16:48:48 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,35 @@
+const sources = [
+  {
+    id: 1,
+    title: "1000 Dokumente",
+    url: "https://www.1000dokumente.de/",
+    provider: "Universität Hamburg",
+    language: "Deutsch",
+    sourceType: "Textquelle",
+    eventCategory: "Deutsche Geschichte",
+    timeCategory: "20. Jahrhundert",
+    description: "Sammlung historischer Dokumente für den Unterricht."
+  },
+  {
+    id: 2,
+    title: "Bundeszentrale für politische Bildung",
+    url: "https://www.bpb.de/",
+    provider: "bpb",
+    language: "Deutsch",
+    sourceType: "Textquelle",
+    eventCategory: "Politik und Geschichte",
+    timeCategory: "Moderne",
+    description: "Didaktisch aufbereitete Inhalte für Schule."
+  },
+  {
+    id: 3,
+    title: "Europeana",
+    url: "https://www.europeana.eu/",
+    provider: "Europeana",
+    language: "Englisch",
+    sourceType: "Bildquelle",
+    eventCategory: "Europäische Geschichte",
+    timeCategory: "Verschiedene Epochen",
+    description: "Digitale Sammlung von Bildern und historischen Quellen."
+  }
+];
